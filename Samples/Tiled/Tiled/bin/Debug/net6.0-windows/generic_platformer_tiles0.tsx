3b13307f71d7750d0c8aec2a150277fe6d63fec7
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2023-02-11 23:42:31 +0800

    Add files via upload

---
+++ Samples/Tiled/Tiled/bin/Debug/net6.0-windows/generic_platformer_tiles0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="generic_platformer_tiles0" tilewidth="16" tileheight="16" tilecount="640" columns="16">
+ <image source="C:/Users/elen8/Downloads/generic_platformer_tiles0.png" width="256" height="640"/>
+</tileset>
